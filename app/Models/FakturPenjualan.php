f034b64ec3ca859393635fc88e13b3f3b44f7206
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-04-29 21:48:54 +0700

    Updated.

--- app/Models/FakturPenjualan.php
+++ app/Models/FakturPenjualan.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Date;
 
 class FakturPenjualan extends Model
 {
@@ -38,7 +39,7 @@ class FakturPenjualan extends Model
 
     public function getNomor(): string
     {
-        return self::NOMOR_PREFIX . $this->getKey();
+        return self::NOMOR_PREFIX . Date::make($this->waktu_pengeluaran)->format("YM") . '-' . $this->getKey();
     }
 
     public function itemFakturPenjualans(): HasMany
